9c4eddb7bfb2d20544cfbb024b93cf76a4d0b823
Momme Jürgensen <momme@juergensen.me>
2017-05-25 10:47:28 +0200

    updated for this year

--- public/main.js
+++ public/main.js
@@ -13,27 +13,38 @@ function convertXml(file, callback) {
   var parser = new xml2js.Parser();
   parser.parseString(xml, function (err, result) {
     if (err) {
+      console.log(err);
       alert("Ein Problem mit der Datei ist aufgetreten.\nEs Könnte vieleicht ein '&' sein.\nBitte überprüfen sie die Datei nach einem '&' oder anderen Fehlern.")
       return;
     }
-    days = result["ljtr-2016"].day
+    days = result["ljtr-2017"].day
     out = "";
     for (var i = 0; i < days.length; i++) {
       events = days[i].event
       if (i === 0) {
-        out = out+"Donnerstag, 05.05.2016\n"
+        out = out+"Montag, 23.05.2017\n"
       } else if (i === 1) {
-        out = out+"Freitag, 06.05.2016\n"
+        out = out+"Mittwoch, 24.05.2017\n"
       } else if (i === 2) {
-        out = out+"Samstag, 07.05.2016\n"
+        out = out+"Donnerstag, 25.05.2017 (Anreisetag)\n"
       } else if (i === 3) {
-        out = out+"Sontag, 08.05.2016\n"
+        out = out+"Freitag, 26.05.2017\n"
+      } else if (i === 4) {
+        out = out+"Samstag, 27.05.2017\n"
+      } else if (i === 5) {
+        out = out+"Sonntag, 28.05.2017 (Abreisetag)\n"
       }
       for (var r = 0; r < events.length; r++) {
-        if (events[r].start[0][0] === "a") {
-          out = out+events[r].start[0]+"\t"+events[r].name+"\t"+events[r].place+"\n";
+        if (events[r].end[0] == "") {
+          out = out+events[r].start[0]+"\t"+
+                events[r].registration+"\t"+
+                events[r].name+"\t"+
+                events[r].place+"\n";
         } else {
-          out = out+events[r].start[0]+" - "+events[r].end[0]+"\t"+events[r].name+"\t"+events[r].place+"\n";
+          out = out+events[r].start[0]+" - "+events[r].end[0]+"\t"+
+                events[r].registration+"\t"+
+                events[r].name+"\t"+
+                events[r].place+"\n";
         }
       }
       out = out+"\n"
